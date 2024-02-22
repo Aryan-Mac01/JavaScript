9b613ac8b125e90d54783e7f580a1c708972d347
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-02-22 22:02:53 +0530

    Do While Loop

---
+++ 03Intermediate/11Loops.js
@@ -0,0 +1,18 @@
+const myStates = [
+    "UP",
+    "MP",
+    "Rajasthan",
+    "Gujarat",
+    1947
+]
+
+// for(let i = 0;i<=myStates.length;i++){
+//     console.log(myStates[i]);
+// }
+
+let i = 0;
+do{
+    console.log(myStates[i]);
+    i++;
+}while(i< myStates.length);
+
