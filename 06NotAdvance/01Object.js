32532e6614a91b5808058694b1d6f003b77e60f1
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-02-27 01:26:02 +0530

    Proto

--- 06NotAdvance/01Object.js
+++ 06NotAdvance/01Object.js
@@ -7,7 +7,13 @@ var User = function (firstName, courseCount){
 
 }
 
+User.prototype.getFirstName = function(){
+    console.log(`Your first name is: ${this.firstName}`);
+};
+
 var Aryan = new User("Aryan", 2); //This create a new user for the properties.
-console.log(Aryan);
+Aryan.getCourseCount(); //2.    
+Aryan.getFirstName();
+///console.log(Aryan);
 
 //Without NEW the 'this' keyword was pointing towards the empty window.
