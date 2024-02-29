49d93edd203d6817ff98f30f865169e084d61c92
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-01 00:07:04 +0530

    Complete JS for Sorting the courses as per price

--- 07Proj02/script.js
+++ 07Proj02/script.js
@@ -21,8 +21,29 @@ const courses = [
 
 function generateLIST(){ //Function to generate List.
     const ul = document.querySelector(".list-group");//Select the class of UL from the HTML Page.
+    ul.innerHTML = ""; //This will reset the ul elements everytime we call the function so that the elements dont get repeated.
     courses.forEach((course) => {
         const li = document.createElement("li");//This will make li element.
         li.classList.add("list-group-item");//This will create the class for the li element.
+
+        const name = document.createTextNode(course.name);
+        li.appendChild(name);
+
+        const span = document.createElement("span");
+        span.classList.add("float-right");
+
+        const price = document.createTextNode("$" + course.price);
+        span.appendChild(price);
+
+        li.appendChild(span);
+        ul.appendChild(li);
     })
 }
+
+window.addEventListener("load", generateLIST);//Calls the function when the website loads.
+
+const button = document.querySelector(".sort-btn");
+button.addEventListener("click", () => {
+    courses.sort((a,b) => a.price - b.price);
+    generateLIST();
+})
