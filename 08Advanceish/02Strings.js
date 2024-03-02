f4e338161dd71f9e43af0fcdaa8e2a64e4afd21f
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-02 23:13:55 +0530

    String

---
+++ 08Advanceish/02Strings.js
@@ -0,0 +1,8 @@
+var name = 'Aryan\'s';
+var fullName = `${name} Mishra`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("aryan"));
+console.log(fullName.toUpperCase());
