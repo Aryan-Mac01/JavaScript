5e25c69532214f5f83e7e176fd8e4e0aabd1f510
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-04 23:52:28 +0530

    Maps

---
+++ 08Advanceish/03Map.js
@@ -0,0 +1,21 @@
+var  myMap = new Map();
+//Maps are like objects but they are not objects, they are comletely different.
+myMap.set(0, "Uno");
+myMap.set(1, "Yes");
+myMap.set(2, "Dos");
+myMap.set(3, "Lol");
+
+console.log(myMap);
+
+for(let key of myMap.keys()){
+    console.log(`key is ${key}`);
+}
+
+for(let value of myMap.values()){
+    console.log(`Value is ${value}`);
+}
+
+
+for(let [key, value] of myMap){
+    console.log(`key is ${key} and value is ${value}`);
+}
