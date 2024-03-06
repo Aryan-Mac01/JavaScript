0685f62620184b4836625b3d7c66017d93c41b65
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-06 14:20:27 +0530

    Spread and Rest operator

--- 08Advanceish/05Spread.js
+++ 08Advanceish/05Spread.js
@@ -4,3 +4,29 @@ console.log(returnedValue);
 var myObj = {};
 Object.assign(myObj, {a:1, b:4, c:2}, {x:9, y:7, z:5});//This is done to assign the key and value inside the object.
 console.log(myObj);
+
+
+function sumOne(a, b){
+    return a+b;
+}
+
+myA = [4, 5, 6];
+console.log(sumOne(...myA));//'... is spread Operator' is used to send the arguments.
+
+//But if the array contains more than 2 value suppose the array is [4, 5, 1], then the function only add the first two values as it only accepts
+//two parameters.
+
+function sumTwo(a,b,...args){ //This is the 'rest operator' and will take as many parameters and arguments as the user provides.
+    //The first two elements of the array will be taken as A and B and the other will come under args.
+    
+    console.log(args);//The 'rest operator' will take the arguments and bunch or bind them into an array.
+    
+    let multi = a*b;
+    let sum = 0;
+    for (const arg of args) {
+        sum+=arg;
+    }
+    return [sum, multi];
+}
+
+console.log(sumTwo(2, 5, 9, 1, 4));
