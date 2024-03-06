ebc598b62e8d352794e7a25006f857156122292f
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-06 13:51:23 +0530

    Object assign

---
+++ 08Advanceish/05Spread.js
@@ -0,0 +1,6 @@
+var returnedValue = Math.max(1,2,5,3,6,3,7,4,6,7,10);//Returns the biggest value.
+console.log(returnedValue);
+
+var myObj = {};
+Object.assign(myObj, {a:1, b:4, c:2}, {x:9, y:7, z:5});//This is done to assign the key and value inside the object.
+console.log(myObj);
