c9e7b78adf773de14c18becf8265e406dbe8682a
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-11 16:31:50 +0530

    Inheritance

--- 08Advanceish/06classjs.js
+++ 08Advanceish/06classjs.js
@@ -20,6 +20,20 @@ class User{
     getCourseList(){//This is a getter, and the main purpose of getter is to return the value.
         return this.#courseList;
     }
+
+    login(){
+        return "You are logged in";
+    }
+}
+
+class SubAdmin extends User{//This is inheritance, a child class extends and uses the properties of base class.
+    constructor(name, email){
+        super(name, email);
+        //The super keyword is used to call the constructor of its parent class to access the parent's properties and methods.
+    }
+    getAdminInfo(){//Static keyword means "Hey, this is a private property, and even sons and grandsons can't inherit this info, stay away"
+        return "I am subadmin";
+    }
 }
 
 module.exports = User;//After exporting you can access this class in any file.
@@ -27,4 +41,9 @@ module.exports = User;//After exporting you can access this class in any file.
 const rock = new User("rock", "rock@gmail.com");
 console.log(rock.getInfo());
 rock.enrollCourse("Angular bootcamp");
-console.log(rock.getCourseList());//Idea of getter is so that not everybody can't access your variable.
+//console.log(rock.getCourseList());//Idea of getter is so that not everybody can't access your variable.
+
+const tom = new SubAdmin("tom", "tom@jerry.com");
+console.log(tom.getAdminInfo());//If getAdminInfo class is statis then we can't access it and this line will throw error.
+console.log(tom.login());
+console.log(tom.getInfo());
