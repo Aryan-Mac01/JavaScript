fa1d7778ebc5b4b9a80a5c7f6fa62315554aeebc
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-17 02:47:16 +0530

    Chuck norris api

--- 08Advanceish/10API.js
+++ 08Advanceish/10API.js
@@ -1,1 +1,13 @@
 //fetch is a already provided by JS that provides you promises everytime.
+fetch('https://api.chucknorris.io/jokes/random')
+    .then((response) => {
+        return response.json();//Then means good response.
+    })
+
+    .then((data) => {
+        var joke = data.value;
+        console.log("Joke", joke);
+    })
+
+    .catch(); //Catch means error.
+    //This code will run on console in web browser.
